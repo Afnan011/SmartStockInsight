ead884b98dd6c6ec865288eb42a2c5dbad21014f
Mahammad Afnan <muhammedafnan8184@gmail.com>
2024-07-02 21:16:59 +0530

    Added some UI improvements

--- app.py
+++ app.py
@@ -129,6 +129,6 @@ def stock_chart_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-    # app.run(debug=True, port=3000)
+    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=3000)
     
